bf7e2489c71372dbe0b9db1bdf293e49d16fc45e
Moki <mokiong1427@gmail.com>
2021-10-21 19:00:08 +0800

    Create routing for search bar

--- pages/events/index.js
+++ pages/events/index.js
@@ -2,13 +2,21 @@ import { getAllEvents } from '../../dummy-data';
 import EventList from '../../components/events/EventList';
 import EventsSearch from '../../components/events/EventsSearch';
 import { Fragment } from 'react';
+import { useRouter } from 'next/router';
 
 function AllEventsPage() {
     const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+
+        router.push(fullPath);
+    }
 
     return (
         <Fragment>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </Fragment>
     );
