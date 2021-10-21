26be7a827d322cbfa0d941effddbaec8468ede9a
Moki <mokiong1427@gmail.com>
2021-10-21 17:55:55 +0800

    Create events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,5 +1,14 @@
+import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/EventList';
+
 function AllEventsPage() {
-    return <div>All events</div>;
+    const events = getAllEvents();
+
+    return (
+        <div>
+            <EventList items={events} />
+        </div>
+    );
 }
 
 export default AllEventsPage;
